7a98860af08d148589e5c6c5cd08d7ccd63a2a97
flino1934 <f.lino1934@hotmail.com>
2021-05-24 13:12:21 -0300

    Preparação inicial para o projeto

---
+++ src/main/java/com/lino/libaryapis/LibaryApisApplication.java
@@ -0,0 +1,13 @@
+package com.lino.libaryapis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibaryApisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibaryApisApplication.class, args);
+	}
+
+}

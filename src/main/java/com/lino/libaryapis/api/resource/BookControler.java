5f678b3b00e6bcea84c98efaf3cd58cfd8816a8f
flino1934 <f.lino1934@hotmail.com>
2021-05-24 23:58:09 -0300

    refatorando a classe test para modelmapper

--- src/main/java/com/lino/libaryapis/api/resource/BookControler.java
+++ src/main/java/com/lino/libaryapis/api/resource/BookControler.java
@@ -3,6 +3,7 @@ package com.lino.libaryapis.api.resource;
 import com.lino.libaryapis.api.dto.BookDto;
 import com.lino.libaryapis.model.entity.Book;
 import com.lino.libaryapis.service.BookService;
+import org.modelmapper.ModelMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,29 +12,22 @@ import org.springframework.web.bind.annotation.*;
 public class BookControler {
 
     private BookService service;
+    private ModelMapper modelMapper;
 
-    public BookControler(BookService service) {//esta fazendo a injção de dependencia via construtor
+    public BookControler(BookService service, ModelMapper modelMapper) {
         this.service = service;
+        this.modelMapper = modelMapper;
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)//vou passar como resposta do sastus o created
     public BookDto create(@RequestBody BookDto dto){
 
-        Book entity = Book.builder()
-                .author(dto.getAuthor())
-                .title(dto.getTitle())
-                .isbn(dto.getIsbn())
-                .build();//recebeu o DTO
+        Book entity = modelMapper.map(dto,Book.class);//vai pegar todos os parametros vindos no dto e transformar em insancia da classe book
 
         entity = service.save(entity);//transformou em entidade e salvou
 
-        return BookDto.builder()
-                .id(entity.getId())
-                .author(entity.getAuthor())
-                .title(entity.getTitle())
-                .isbn(entity.getIsbn())
-                .build();//retornou um DTO com as entidades salvas
+        return modelMapper.map(entity,BookDto.class);//retornou um DTO com as entidades salvas
     }
 
 
